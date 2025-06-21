bffef053223e01ca4ecd38e65ad6293fc7a42956
prasanna2126 <151775082+prasanna2126@users.noreply.github.com>
2025-06-21 13:32:40 +0530

    App.js

---
+++ client/Src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
+import PatientDashboard from "./components/Dashboard/PatientDashboard";
+import PrivateRoute from "./utils/PrivateRoute";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <PatientDashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
